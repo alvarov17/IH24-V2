a47076d80c129c55226ba37487d0c1f4474417af
Robert Gilmore <robbiefive@hotmail.com>
2024-01-28 10:01:32 -0800

    working on send page
    unfinished

--- static/home.js
+++ static/home.js
@@ -1,26 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log('loaded')
-    /*
-    const viewRefresher = document.getElementById("view");
-    console.log('test1')
-        viewRefresher.addEventListener("submit", function(e) {
-            console.log('test2')
-            e.preventDefault()
-        });
-    */
     const view = document.getElementById("view");
     view.addEventListener("click", function() {
         window.location.href = "/view";
     });
     const send = document.getElementById("send");
     send.addEventListener("click", function() {
-        console.log('asdfasdf2')
         window.location.href = "/send";
     });
 
     const edit = document.getElementById("edit");
     edit.addEventListener("click", function() {
-        console.log('asdfasdf3')
         window.location.href = "/edit";
+    });
+    const back = document.getElementById("back-btn");
+    back.addEventListener("click", function() {
+        window.location.href = "/home";
     });
 });
